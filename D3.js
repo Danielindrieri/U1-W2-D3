0f98a1aab11574f534e22268bb0f2016d70c1c4c
Daniel Indrieri <indrieridaniel@gmail.com>
2024-10-09 17:59:25 +0200

    adds

--- D3.js
+++ D3.js
@@ -202,8 +202,8 @@ if (crewMass < 500) {
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
 */
 for (let i = 0; i < starWarsCharacters.length; i++) {
-  if (delete starWarsCharacters.gender === 'n/a') {
-    starWarsCharacters.gender.push(starWarsCharacters.gender === 'robot')
+  if (starWarsCharacters[i].gender === 'n/a') {
+    (starWarsCharacters[i].gender === 'robot')
   }
 }
 console.log(starWarsCharacters)
